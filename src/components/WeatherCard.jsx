7a93ea736b43618103ba4726e28f5d43d1a25ba7
Elie BULAKALI <137182964+ElieBulakali@users.noreply.github.com>
2026-01-02 16:39:49 +0100

    Change WeatherCard background color to blue

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,7 +6,7 @@ const WeatherCard = () => {
   if (!weather) return null;
 
   return (
-    <div className="bg-white dark:bg-gray-900 p-4 rounded-xl shadow">
+    <div className="bg-blue dark:bg-gray-900 p-4 rounded-xl shadow">
       <h3 className="text-lg font-semibold mb-2">🌤 Weather</h3>
 
       <p className="text-sm text-gray-500">{weather.city}</p>
